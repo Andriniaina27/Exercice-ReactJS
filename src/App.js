9873d382e5b520e2a263213046eb8ccfe62acea1
Andriniaina Luca <ranaivoluca3@gmail.com>
2025-10-05 13:54:54 +0200

    App Todo-List complet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import './App.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const [form, setForm] = useState({
+    add: ""
+  });
+
+  const [people, setPeople] = useState([]);
+  const [error, setError] = useState("");
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleAdd = () =>{
+    const {add} = form
+
+    if (!add){
+      setError("Veuillez remplir le champ vide");
+      return;
+    }
+
+    setPeople([...people, {add}]);
+    setForm({add: ""});
+    setError("");
+  }
+
+  const handleDelete = (index) => {
+    setPeople(people.filter((_, i) => i !== index));
+  }
   return (
     <div className="App">
       <header className="App-header">
         <div className="App-task">
           <h1>Todo List</h1>
           <div class="heading">
-            <input type="text" name="add" id="add-task"/>
-            <button type="button" class="add" id="btn-add">tâche</button>
+            <input type="text" name="add" placeholder='ajouter une tâche' onChange={handleChange} value={form.add} id="add-task"/>
+            <button type="button" class="add" id="btn-add" onClick={handleAdd}><FontAwesomeIcon icon={faPlus} /></button>
           </div>
-          <div class="list-task">
-            <p>Tâche 1</p>
-            <button type="button" class="delete">Supprimer</button>
-          </div>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+            {people.map((person, index) =>(
+              <div class="list-task" key={index}>
+                <p>{person.add}</p>
+                <button type="button" class="delete" onClick={() => handleDelete(index)}><FontAwesomeIcon icon={faTrash} /></button>
+              </div>
+            ))}
         </div>
       </header>
     </div>
